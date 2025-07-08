c6ac6457a91d09d686c3f8b7f9273096bb711014
João Pedro <joaogapires@gmail.com>
2025-07-08 20:18:09 -0300

    CSS básico & cartões interativos
    feat: adição dos cartões interativos
    feat: modificação da nav para ser fixa
    feat: adição de cores na página
    feat: modificação no css da intro na página
    ----------------------------------------
    fix: exclusão dos múltiplos cartões para tirar conflitos no js

--- js/script.js
+++ js/script.js
@@ -22,3 +22,46 @@
     function closeModal() {
       document.getElementById('modal-bg').style.display = 'none';
     }
+
+    const cards = document.querySelectorAll('.card');
+    const popup = document.getElementById('project-popup');
+
+    const popupTitle = document.getElementById('popup-title');
+    const popupDescription = document.getElementById('popup-description');
+    const authorName = document.getElementById('author-name');
+    const authorGithub = document.getElementById('author-github');
+    const authorLinkedin = document.getElementById('author-linkedin');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    const title = card.getAttribute('data-nome');
+    const author = card.getAttribute('data-autor');
+    const longDesc = card.getAttribute('data-desc');
+    const projectImg = card.getAttribute('data-projimg');
+    const devImg = card.getAttribute('data-devimg');
+    const projectLink = card.getAttribute('data-link');
+    const githubUrl = card.querySelector('a[href*="github.com"]').href;
+    const linkedinUrl = card.querySelector('a[href*="linkedin.com"]').href;
+
+    popupTitle.innerText = title;
+    document.getElementById('author-name').innerText = author;
+    document.getElementById('popup-description').innerText = longDesc;
+    document.getElementById('popup-project-img').src = projectImg;
+    document.getElementById('popup-dev-photo').src = devImg;
+    document.getElementById('popup-project-link').href = projectLink;
+    document.getElementById('author-github').href = githubUrl;
+    document.getElementById('author-linkedin').href = linkedinUrl;
+
+    popup.classList.add('open');
+  });
+});
+
+    function closePopup() {
+    popup.classList.remove('open');
+    }
+
+    popup.addEventListener('click', (e) => {
+    if (e.target === popup) {
+        closePopup();
+    }
+    });
